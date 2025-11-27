e2dfa45e5805027f76ef2873c11706affea86657
lyolek <lyolek@gmail.com>
2025-11-27 10:51:32 +0200

    Губився DictWord

--- src/main/java/org/sav/cardsback/mapper/WordMapper.java
+++ src/main/java/org/sav/cardsback/mapper/WordMapper.java
@@ -1,7 +1,9 @@
 package org.sav.cardsback.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.sav.cardsback.dto.WordDto;
+import org.sav.cardsback.entity.DictWord;
 import org.sav.cardsback.entity.Word;
 
 import java.util.List;
@@ -9,9 +11,20 @@ import java.util.List;
 @Mapper(componentModel = "spring", uses = {WordStateMapper.class})
 public interface WordMapper {
 
-	WordDto toDto(Word word);
+	@Mapping(target = "dictWordId", source = "dictWord.id")
+	WordDto toDto(Word entity);
 
-	List<WordDto> toDtoList(List<Word> words);
+	List<WordDto> toDtoList(List<Word> entities);
 
-	Word toEntity(WordDto wordDto);
+	@Mapping(target = "dictWord", source = "dictWordId")
+	Word toEntity(WordDto dto);
+
+	List<Word> toEntityList(List<WordDto> dtos);
+	default DictWord mapDictWord(long id) {
+		if (id == 0) return null; // Dto може передати 0, тому підстрахуємо
+		DictWord d = new DictWord();
+		d.setId(id);
+		return d;
+	}
+
 }
