d6d42a393805bbaf60c614ae4ef5048f260c78c9
lyolek <lyolek@gmail.com>
2025-12-05 16:42:12 +0200

    Виправлення помилки перевірки по Word у isWordSuitable

--- src/main/java/org/sav/cardsback/domain/dictionary/repository/DictionaryRepository.java
+++ src/main/java/org/sav/cardsback/domain/dictionary/repository/DictionaryRepository.java
@@ -1,14 +1,12 @@
 package org.sav.cardsback.domain.dictionary.repository;
 
 import org.sav.cardsback.entity.DictWord;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface DictionaryRepository extends JpaRepository<DictWord, Long> {
@@ -21,13 +19,18 @@ public interface DictionaryRepository extends JpaRepository<DictWord, Long> {
 	void deleteByWordText(String wordText);
 
 	@Query("""
-    select case when count(udw) > 0 or count(w) > 0 then true else false end
-        from UserDictWord udw
-        left join Word w
-            on w.userId = :userId
-            and w.dictWord.id = :dictWordId
-        where udw.userId = :userId
-            and udw.lemma.id = :dictWordId
+    select
+        (exists (
+            select 1 from UserDictWord udw
+            where udw.userId = :userId
+              and udw.lemma.id = :dictWordId
+        )
+        or
+        exists (
+            select 1 from Word w
+            where w.userId = :userId
+              and w.dictWord.id = :dictWordId
+        ))
     """)
 	boolean existsByUserAndDictWord(@Param("userId") Long userId, @Param("dictWordId") Long dictWordId);
 
